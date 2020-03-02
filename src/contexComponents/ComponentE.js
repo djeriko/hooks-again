4690d289f75a29445ef58e5f605332ad6be2abcc
djeriko <jerikosalivan@gmail.com>
2020-03-02 18:42:39 +0900

    useContext

--- src/contexComponents/ComponentE.js
+++ src/contexComponents/ComponentE.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import ComponentF from './ComponentF'
+import { UserContext, ChannelContext } from '../App'
 
 function ComponentE() {
+
+  const user = useContext(UserContext)
+  const chanenel = useContext(ChannelContext)
+
   return (
     <div>
      <ComponentF/> 
+     {user} - {chanenel}
     </div>
   )
 }
