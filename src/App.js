d51852c2340b8d29ec9fd5083e8eeb8b45d511af
djeriko <jerikosalivan@gmail.com>
2020-03-02 17:31:59 +0900

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HooksCounter from './components/HooksCounter';
-import HooksCounterTwo from './components/HooksCounterTwo';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterOne from './components/HookCounterOne';
+import React from "react"
+import "./App.css"
+import DataFetching from "./components/DataFetching"
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounter/> */}
-      {/* <HooksCounter/> */}
-      {/* <HooksCounterTwo/> */}
-      {/* <HookCounterThree/> */}
-      <HookCounterOne/>
-      {/* <HookCounterFour/> */}
+      <DataFetching/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
