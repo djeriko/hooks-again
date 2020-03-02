a85d22d4084ae1ec6aabb7f30eb11bf56baab780
djeriko <jerikosalivan@gmail.com>
2020-03-02 17:48:13 +0900

    fetching data onClick event

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,24 +2,36 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 function DataFetching() {
-  const [posts, setPosts] = useState([])
+  const [post, setPost] = useState({})
+  const [id, setId] = useState(1)
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1)
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then(res => {
         console.log(res)
-        setPosts(res.data)
+        setPost(res.data)
       })
       .catch(err => {
         console.log(err)
       })
-  }, [])
+  }, [idFromButtonClick])
+
+  const onClickHandler = () => {
+    setIdFromButtonClick(id)
+  }
+
   return (
     <div>
-      {posts.map(post => (
+      <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button type="button" onClick={onClickHandler}>
+        Fetch Post
+      </button>
+      <div>{post.title}</div>
+      {/* {posts.map(post => (
         <li key={post.id}>{post.title}</li>
-      ))}
+      ))} */}
     </div>
   )
 }
