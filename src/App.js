ed2073ef9881588ee853869bcef600a90ce1dd7f
djeriko <jerikosalivan@gmail.com>
2020-03-06 20:01:40 +0900

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import React, { useReducer } from "react"
 import "./App.css"
-import ComponentA from "./contexReducerComponents/ComponentA"
-import ComponentB from "./contexReducerComponents/ComponentB"
-import ComponentC from "./contexReducerComponents/ComponentC"
-import DataFetchingOne from "./dataFetchComponents/DataFetchingOne"
-import DataFetchingTwo from "./dataFetchComponents/DataFetchingTwo"
-
-export const CountContext = React.createContext()
-
-const initialState = 0
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1
-    case "decrement":
-      return state - 1
-    case "reset":
-      return initialState
-    default:
-      return state
-  }
-}
+import ParentComponent from './callbackComponents/ParentComponent.js'
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    // <CountContext.Provider
-    //   value={{ countState: count, countDispatch: dispatch }}
-    // >
-    //   <div className="App">
-    //     Count - {count}
-    //     <ComponentA />
-    //     <ComponentB />
-    //     <ComponentC />
-    //   </div>
-    // </CountContext.Provider>
-    <DataFetchingTwo/>
+    <div className="App">
+      <ParentComponent/>
+    </div>
   )
 }
 
